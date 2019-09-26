27d5e1234b403535b5532787fb4448baabcec591
Yaseminp <53197333+Yaseminp@users.noreply.github.com>
2019-09-26 13:35:09 -0700

    User model and  API

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require ('./routes/api/profile');
+const posts = require ('./routes/api/posts');
+
 const app = express();
+
+
 
 //DB config
 const db = require('./config/keys').mongoURI
@@ -13,6 +19,13 @@ mongoose
 
 //First route
 app.get('/', (req,res) => res.send('Hello!'));
+
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
 const port = 7100;
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
+
+
 
